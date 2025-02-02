c12a9d574245ea5428d42954c220960501e5cec5
Ermak0vYurii <grantovod1@gmail.com>
2025-02-02 17:37:57 +0500

    add MainPage.java

---
+++ src/main/java/pageobject/MainPage.java
@@ -0,0 +1,13 @@
+package pageobject;
+
+import org.openqa.selenium.WebDriver;
+
+public class MainPage {
+
+    private WebDriver driver;
+
+    public static final String URL = "https://stellarburgers.nomoreparties.site/";
+
+
+
+}
