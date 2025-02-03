720d736964b683b37e36dbacd8b88f06e48f298f
Ermak0vYurii <grantovod1@gmail.com>
2025-02-03 21:11:13 +0500

    добавлены локаторы в RegisterPage

--- src/main/java/pageobject/RegisterPage.java
+++ src/main/java/pageobject/RegisterPage.java
@@ -1,12 +1,25 @@
 package pageobject;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class RegisterPage {
 
     private WebDriver driver;
 
+    //поле Имя
+    private final By inputName = By.xpath(".//label[text()='Имя']");
+    //поле Email
+    private final By inputEmail = By.xpath(".//label[text()='Email']");
+    //поле Пароль
+    private final By inputPassword = By.xpath(".//input[@name='Пароль']");
+    //кнопка зарегистрироваться
+    private final By registerButton = By.xpath(".//button[text()='Зарегистрироваться']");
+
     public RegisterPage(WebDriver driver) {
         this.driver = driver;
     }
+
+    //методы
+
 }
