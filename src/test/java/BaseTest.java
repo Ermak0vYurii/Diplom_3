de866143698235981fe77698ae99448aeee68c5e
Ermak0vYurii <grantovod1@gmail.com>
2025-02-03 14:57:18 +0500

    add BaseTest.java настройка браузера и открытие приложения

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,35 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static pageobject.MainPage.URL;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+    String browser = "chrome";
+
+    @Before
+    public void prepare() {
+        if (browser.equalsIgnoreCase("yandex")) {
+            System.setProperty("webdriver.chrome.driver", "C:/WebDriver/bin/chromedriver.exe");
+            ChromeOptions options = new ChromeOptions();
+            options.setBinary("C:\\Users\\pocht\\AppData\\Local\\Yandex\\YandexBrowser\\Application\\browser.exe");
+            driver = new ChromeDriver(options);
+        } else if (browser.equalsIgnoreCase("chrome")) {
+            driver = new ChromeDriver();
+            WebDriverManager.chromedriver().setup();
+        }
+
+        driver.get(URL);
+
+    }
+
+    @After
+    public void cleanUp() {
+        driver.quit();
+    }
+}
