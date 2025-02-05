a1f7b616b27feab4a074f18eedd3a8939843dbb0
Ermak0vYurii <grantovod1@gmail.com>
2025-02-05 15:01:23 +0500

    Написан первый тест на авторизацию

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,10 +1,17 @@
 import api.UserClient;
+import io.qameta.allure.junit4.DisplayName;
 import io.restassured.response.Response;
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import pageobject.LoginPage;
 import pageobject.MainPage;
 import pageobject.RegisterPage;
 import pojo.User;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
 
 public class LoginTest extends BaseTest {
 
@@ -23,6 +30,23 @@ public class LoginTest extends BaseTest {
         client = new UserClient();
         user = new User("user-test@ya.ru", "password", "Ivan");
         Response response = client.createUser(user);
-        token = response.
+        token = response.then().extract().path("accessToken");
+    }
+
+    @After
+    public void cleanUp() {
+        if(token != null) {
+            client.deleteUser(token);
+        }
+    }
+
+    @Test
+    @DisplayName("Вход по кнопке \"Войти в аккаунт\" на главной странице")
+    public void loginToAccountButtonTest() {
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        objMainPage.clickLoginButton();
+        objLoginPage.loginUser(user.getEmail(), user.getPassword());
+        objMainPage.waitLoadMainPage();
+        assertEquals(URL,driver.getCurrentUrl());
     }
 }
