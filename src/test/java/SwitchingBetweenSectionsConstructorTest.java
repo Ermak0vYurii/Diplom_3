8dc31fcb1a313bded2109cbbcdf0fb35efcbd939
Ermak0vYurii <grantovod1@gmail.com>
2025-02-06 21:55:58 +0500

    готово

--- src/test/java/SwitchingBetweenSectionsConstructorTest.java
+++ src/test/java/SwitchingBetweenSectionsConstructorTest.java
@@ -14,7 +14,8 @@ public class SwitchingBetweenSectionsConstructorTest extends BaseTest {
         objMainPage = new MainPage(driver);
         objMainPage.clickSaucesButton();
         objMainPage.clickBunsButton();
-        assertTrue(objMainPage.listBunsIsDisplayed());
+        String className = objMainPage.getClassBunsSection();
+        assertTrue(className.contains("type_current"));
     }
 
     @Test
@@ -23,9 +24,45 @@ public class SwitchingBetweenSectionsConstructorTest extends BaseTest {
         objMainPage = new MainPage(driver);
         objMainPage.clickFillingsButton();
         objMainPage.clickBunsButton();
-        assertTrue(objMainPage.listBunsIsDisplayed());
+        String className = objMainPage.getClassBunsSection();
+        assertTrue(className.contains("type_current"));
     }
 
+    @Test
+    @DisplayName("Переход к разделу Соусы из раздела Булки")
+    public void switchingSectionsBunsToSaucesTest() {
+        objMainPage = new MainPage(driver);
+        objMainPage.clickSaucesButton();
+        String className = objMainPage.getClassSaucesSection();
+        assertTrue(className.contains("type_current"));
+    }
 
+    @Test
+    @DisplayName("Переход к разделу Соусы из раздела Начинки")
+    public void switchingSectionsFillingsToSaucesTest() {
+        objMainPage = new MainPage(driver);
+        objMainPage.clickFillingsButton();
+        objMainPage.clickSaucesButton();
+        String className = objMainPage.getClassSaucesSection();
+        assertTrue(className.contains("type_current"));
+    }
 
+    @Test
+    @DisplayName("Переход к разделу Начинки из раздела Булки")
+    public void switchingSectionsBunsToFillingsTest() {
+        objMainPage = new MainPage(driver);
+        objMainPage.clickFillingsButton();
+        String className = objMainPage.getClassFillingsSection();
+        assertTrue(className.contains("type_current"));
+    }
+
+    @Test
+    @DisplayName("Переход к разделу Начинки из раздела Соусы")
+    public void switchingSectionsSaucesToFillingsTest() {
+        objMainPage = new MainPage(driver);
+        objMainPage.clickSaucesButton();
+        objMainPage.clickFillingsButton();
+        String className = objMainPage.getClassFillingsSection();
+        assertTrue(className.contains("type_current"));
+    }
 }
