bbdeeccc48b34b7fb19f68674b201fbb82cece0b
Ermak0vYurii <grantovod1@gmail.com>
2025-02-06 18:48:56 +0500

    update MainPage.java добавлены локаторы

--- src/main/java/pageobject/MainPage.java
+++ src/main/java/pageobject/MainPage.java
@@ -16,6 +16,12 @@ public class MainPage {
     private final By loginButton = By.xpath(".//button[text()='Войти в аккаунт']");
     //кнопка Оформить заказ
     private final By createOrderButton = By.xpath(".//button[text()='Оформить заказ']");
+    //кнопка выбора раздела конструктора Булки
+    private final By sectionBunsButton = By.xpath(".//span[text()='Булки']");
+    //кнопка выбора раздела конструктора Соусы
+    private final By sectionSaucesButton = By.xpath(".//span[text()='Соусы']");
+    //кнопка выбора раздела конструктора Начинки
+    private final By sectionFillingsButton = By.xpath(".//span[text()='Начинки']");
 
     public MainPage(WebDriver driver) {
         this.driver = driver;
@@ -35,6 +41,21 @@ public class MainPage {
     public void waitLoadMainPage() {
         new WebDriverWait(driver, 3).
                 until(ExpectedConditions.visibilityOfElementLocated(createOrderButton));
+    }
+
+    @Step("Нажатие на раздел конструктора Булки")
+    public void clickBunsButton() {
+        driver.findElement(sectionBunsButton).click();
+    }
+
+    @Step("Нажатие на раздел конструктора Соусы")
+    public void clickSaucesButton() {
+        driver.findElement(sectionSaucesButton).click();
+    }
+
+    @Step("Нажатие на раздел конструктора Булки")
+    public void clickFillingsButton() {
+        driver.findElement(sectionFillingsButton).click();
     }
 
 }
