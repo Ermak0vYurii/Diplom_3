87008cb5852264cb6a6e3873afa4f36abcfbddcd
Ermak0vYurii <grantovod1@gmail.com>
2025-02-06 19:17:29 +0500

    написан переход в Булки

--- src/test/java/SwitchingBetweenSectionsConstructorTest.java
+++ src/test/java/SwitchingBetweenSectionsConstructorTest.java
@@ -1,8 +1,30 @@
+import io.qameta.allure.junit4.DisplayName;
+import org.junit.Test;
 import pageobject.MainPage;
+
+import static org.junit.Assert.assertTrue;
 
 public class SwitchingBetweenSectionsConstructorTest extends BaseTest {
 
     MainPage objMainPage;
+
+    @Test
+    @DisplayName("Переход к разделу Булки из раздела Соусы")
+    public void switchingSectionsSaucesToBunsTest() {
+        objMainPage = new MainPage(driver);
+        objMainPage.clickSaucesButton();
+        objMainPage.clickBunsButton();
+        assertTrue(objMainPage.listBunsIsDisplayed());
+    }
+
+    @Test
+    @DisplayName("Переход к разделу Булки из раздела Начинки")
+    public void switchingSectionsFillingsToBunsTest() {
+        objMainPage = new MainPage(driver);
+        objMainPage.clickFillingsButton();
+        objMainPage.clickBunsButton();
+        assertTrue(objMainPage.listBunsIsDisplayed());
+    }
 
 
 
